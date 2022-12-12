d6333f5cc7d0b6f935f4388706895e23193a3e9b
nuandawm <derossi.giuliano@gmail.com>
2022-12-12 20:03:11 +0100

    Add scatter plot component

--- spa/src/app/app.module.ts
+++ spa/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { D3ScatterPlotComponent } from './d3-scatter-plot/d3-scatter-plot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    D3ScatterPlotComponent
   ],
   imports: [
     BrowserModule,
