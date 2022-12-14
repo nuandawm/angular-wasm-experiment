723d5f1cb774ae93cf59ab53a7c2a02c600c83ff
nuandawm <derossi.giuliano@gmail.com>
2022-12-14 12:53:52 +0100

    First try to render data with D3 and SVG: too slow

--- spa/src/app/d3-scatter-plot/d3-scatter-plot.component.ts
+++ spa/src/app/d3-scatter-plot/d3-scatter-plot.component.ts
@@ -1,51 +1,132 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as d3 from 'd3';
 import { Selection } from 'd3';
 import { _3d } from 'd3-3d';
 
 const ScatterPlot = (function(d3SvgElement: Selection<any, any, any, any>){
   const START_ANGLE = Math.PI/4;
+  const INITAL_SCALE = 20;
+  const ORIGIN = [300, 200];
+  const SCATTER_POINT_SIZE = 1;
   let mouseX: number;
   let mouseY: number;
   let mx: number;
   let my: number;
-  let xGridData: Array<Array<any>> = [];
+  let alpha: number;
+  let beta: number;
+  let xGridData: Array<Array<number>> = [];
+  let xScaleData: Array<Array<number>> = [];
+  let yScaleData: Array<Array<number>> = [];
+  let zScaleData: Array<Array<number>> = [];
+  let scatterPointsData: Array<{x: number, y: number, z: number, id: string}> = [];
 
   const grid3d = _3d()
     .shape('GRID', 20)
-    .origin([300, 200]) // origin
+    .origin(ORIGIN)
     .rotateY( START_ANGLE) //  startAngle
     .rotateX(-START_ANGLE) // -startAngle
-    .scale(20); // initialScale
+    .scale(INITAL_SCALE);
 
-  const processData = (grid3dData: any, tt: number) => {
-    const xGrid: Selection<any, any, any, any> = d3SvgElement
-      .selectAll('path.grid')
-      .data(grid3dData, (d: any) => d.id);
-    xGrid
+  const point3d = _3d()
+    .x(function(d: any){ return d.x; })
+    .y(function(d: any){ return d.y; })
+    .z(function(d: any){ return d.z; })
+    .origin(ORIGIN)
+    .rotateY( START_ANGLE)
+    .rotateX(-START_ANGLE)
+    .scale(INITAL_SCALE);
+
+  const yScale3d = _3d()
+    .shape('LINE_STRIP')
+    .origin(ORIGIN)
+    .rotateY( START_ANGLE)
+    .rotateX(-START_ANGLE)
+    .scale(INITAL_SCALE);
+
+  function posPointX(d: any){
+    return d.projected.x;
+  }
+  function posPointY(d: any){
+    return d.projected.y;
+  }
+
+  const hideAxes = () => {
+    d3SvgElement.selectAll('path.axis').style('display', 'none');
+  }
+  const processAxesData = (yScale3dData: any) => {
+    /* ----------- Axis ----------- */
+    const yScale: Selection<any, any, any, any> = d3SvgElement.selectAll('path.axis').data(yScale3dData);
+    yScale
       .enter()
       .append('path')
-      .attr('class', '_3d grid')
-      .merge(xGrid)
+      .attr('class', '_3d axis')
+      .merge(yScale)
       .attr('stroke', 'black')
-      .attr('stroke-width', 0.3)
-      .attr('fill', d => d.ccw ? 'lightgrey' : '#717171')
-      .attr('fill-opacity', 0.9)
-      .attr('d', grid3d.draw);
+      .attr('stroke-width', .5)
+      .attr('d', yScale3d.draw)
+      // show axes
+      .style('display', 'inline');
+    yScale.exit().remove();
+  }
+
+  const hideElements = () => {
+    d3SvgElement.selectAll('path.grid').style('display', 'none');
+    d3SvgElement.selectAll('circle.point').style('display', 'none');
+  }
+  const processData = (grid3dData: any, pointsData: any) => {
+    /* ----------- x-Grid ----------- */
+    const xGrid: Selection<any, any, any, any> = d3SvgElement
+      .selectAll('path.grid')
+      .data(grid3dData, (d: any) => d.plane);
+    xGrid
+      .enter()
+        .append('path')
+        .attr('class', '_3d grid')
+      .merge(xGrid)
+        .attr('stroke', 'black')
+        .attr('stroke-width', 0.3)
+        .attr('fill', d => d.ccw ? 'lightgrey' : '#717171')
+        .attr('fill-opacity', 0.9)
+        .attr('d', grid3d.draw)
+        // show elements
+        .style('display', 'inline');
     xGrid.exit().remove();
+
+    /* ----------- POINTS ----------- */
+    const points: Selection<any, any, any, any> = d3SvgElement.selectAll('circle.point').data(pointsData, ({ id }) => id);
+    points
+      .enter()
+        .append('circle')
+        .attr('class', '_3d point')
+        .attr('opacity', 0)
+        .attr('cx', posPointX)
+        .attr('cy', posPointY)
+      .merge(points)
+        .attr('r', SCATTER_POINT_SIZE)
+        .attr('fill', 'black')
+        .attr('opacity', 1)
+        .attr('cx', posPointX)
+        .attr('cy', posPointY)
+        // show elements
+        .style('display', 'inline');
+    points.exit().remove();
+
+    // ???
     d3.selectAll('._3d').sort(_3d().sort);
   }
 
   const dragged = (event: any) => {
     mouseX = mouseX || 0;
     mouseY = mouseY || 0;
-    const beta   = (event.x - mx + mouseX) * Math.PI / 230 ;
-    const alpha  = (event.y - my + mouseY) * Math.PI / 230  * (-1);
-    const grid3dData = grid3d
+    beta   = (event.x - mx + mouseX) * Math.PI / 230 ;
+    alpha  = (event.y - my + mouseY) * Math.PI / 230  * (-1);
+    // maybe this could be elaborated only on dragEnd?
+    const yScale3dData = yScale3d
       .rotateY(beta + START_ANGLE)
       .rotateX(alpha - START_ANGLE)
-      (xGridData);
-    processData(grid3dData, 0);
+      ([xScaleData, yScaleData, zScaleData]);
+    hideElements();
+    processAxesData(yScale3dData);
   }
 
   const dragStarted = (event: any) => {
@@ -56,6 +137,16 @@ const ScatterPlot = (function(d3SvgElement: Selection<any, any, any, any>){
   const dragEnded = (event: any) => {
     mouseX = event.x - mx + mouseX;
     mouseY = event.y - my + mouseY;
+    hideAxes();
+    const grid3dData = grid3d
+      .rotateY(beta + START_ANGLE)
+      .rotateX(alpha - START_ANGLE)
+      (xGridData);
+    const scatterPoints3dData = point3d
+      .rotateY(beta + START_ANGLE)
+      .rotateX(alpha - START_ANGLE)
+      (scatterPointsData);
+    processData(grid3dData, scatterPoints3dData);
   }
 
   // Init
@@ -69,13 +160,32 @@ const ScatterPlot = (function(d3SvgElement: Selection<any, any, any, any>){
 
   for (let z = -10; z < 10; z++) {
     for (let x = -10; x < 10; x++) {
-      xGridData.push([x, 1, z]);
+      xGridData.push([x, 0, z]);
+      scatterPointsData.push({
+        x, y: -2, z, id: `cnt_${x}_${z}`
+      });
+    }
+  }
+  d3.range(0, 5, 1).forEach(function(d){ xScaleData.push([-d, 0, 0]); });
+  d3.range(0, 5, 1).forEach(function(d){ yScaleData.push([0, -d, 0]); });
+  d3.range(0, 5, 1).forEach(function(d){ zScaleData.push([0, 0, d]); });
+
+  const pointsPerAxis = 80;
+  for (let z = -(pointsPerAxis / 2); z < (pointsPerAxis / 2); z++) {
+    for (let x = -(pointsPerAxis / 2); x < (pointsPerAxis / 2); x++) {
+      scatterPointsData.push({
+        x: x * 20 / pointsPerAxis,
+        y: -2,
+        z: z * 20 / pointsPerAxis, id: `cnt_${x}_${z}`
+      });
     }
   }
 
+  const grid3dData = grid3d(xGridData);
+  const scatterPoints3dData = point3d(scatterPointsData);
   processData(
-    grid3d(xGridData),
-    1000
+    grid3dData,
+    scatterPoints3dData
   );
 })
 
@@ -84,93 +194,16 @@ const ScatterPlot = (function(d3SvgElement: Selection<any, any, any, any>){
   templateUrl: './d3-scatter-plot.component.html',
   styleUrls: ['./d3-scatter-plot.component.scss']
 })
-export class D3ScatterPlotComponent implements OnInit, AfterViewInit {
-  private static START_ANGLE = Math.PI/4;
+export class D3ScatterPlotComponent implements OnInit {
 
   @ViewChild('svgElement', {static: true}) svgElement: ElementRef | undefined;
 
-  private d3SvgElement: Selection<any, any, any, any> | undefined;
-  private mx: any;
-  private my: any;
-  private mouseX: any;
-  private mouseY: any;
-  private xGridData: any;
-  private grid3d = _3d()
-    .shape('GRID', 20)
-    .origin([300, 200]) // origin
-    .rotateY( D3ScatterPlotComponent.START_ANGLE) //  startAngle
-    .rotateX(-D3ScatterPlotComponent.START_ANGLE) // -startAngle
-    .scale(20); // initialScale
-
   constructor() { }
 
-  private dragged(event: any) {
-    this.mouseX = this.mouseX || 0;
-    this.mouseY = this.mouseY || 0;
-    const beta   = (event.x - this.mx + this.mouseX) * Math.PI / 230 ;
-    const alpha  = (event.y - this.my + this.mouseY) * Math.PI / 230  * (-1);
-    const grid3dData = this.grid3d
-      .rotateY(beta + D3ScatterPlotComponent.START_ANGLE)
-      .rotateX(alpha - D3ScatterPlotComponent.START_ANGLE)
-      (this.xGridData);
-    this.processData(grid3dData, 0);
-  }
-
-  private dragStarted(event: any) {
-    this.mx = event.x;
-    this.my = event.y;
-  }
-
-  private dragEnded(event: any) {
-    this.mouseX = event.x - this.mx + this.mouseX;
-    this.mouseY = event.y - this.my + this.mouseY;
-  }
-
-  private processData(grid3dData: any, tt: number) {
-    if (this.d3SvgElement) {
-      const xGrid: Selection<any, any, any, any> = this.d3SvgElement
-        .selectAll('path.grid')
-        .data(grid3dData, (d: any) => d.id);
-      xGrid
-        .enter()
-        .append('path')
-        .attr('class', '_3d grid')
-        .merge(xGrid)
-        .attr('stroke', 'black')
-        .attr('stroke-width', 0.3)
-        .attr('fill', d => d.ccw ? 'lightgrey' : '#717171')
-        .attr('fill-opacity', 0.9)
-        .attr('d', this.grid3d.draw);
-      xGrid.exit().remove();
-      d3.selectAll('._3d').sort(_3d().sort);
-    }
-  }
-
   ngOnInit(): void {
-    this.d3SvgElement = d3.select(this.svgElement?.nativeElement);
-    this.d3SvgElement
-      .attr('width', 600)
-      .attr('height', 450)
-      .call(d3.drag()
-        .on('drag', event => this.dragged(event))
-        .on('start', event => this.dragStarted(event))
-        .on('end', event => this.dragEnded(event)))
-      .append('g');
-
-    this.xGridData = [];
-    for (let z = -10; z < 10; z++) {
-      for (let x = -10; x < 10; x++) {
-        this.xGridData.push([x, 1, z]);
-      }
-    }
-
-    this.processData(
-      this.grid3d(this.xGridData),
-      1000
+    ScatterPlot(
+      d3.select(this.svgElement?.nativeElement)
     );
-  }
-
-  ngAfterViewInit() {
   }
 
 }
